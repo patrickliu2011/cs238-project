46193d1a19c3262b01a1eeff2da2cc0bb9f5a818
Patrick Liu <pliu1@stanford.edu>
2023-03-06 03:18:20 -0800

    Reduce experiments, run in series

--- make_configs.py
+++ make_configs.py
@@ -2,12 +2,12 @@ from itertools import product
 
 final_command = 'python3 train_dqn.py '
 
-sizes = {4, 6}
+sizes = {5}
 slip = {True, False}
 num_episode = {1000}
 eval_episode = {100}
 reward_overrides = {"H:0 F:0 G:1", "H:-1 F:-0.01 G:1", "H:-1 F:0.1 G:1"}
-gamma = {0.9, 0.99}
+gamma = {0.99}
 ratio_hide = {0.5, 1}
 state_type = {"map"}
 
@@ -35,6 +35,11 @@ print(config_lists)
 
 commands = config_lists
 
+import subprocess
+for cmd in commands:
+    subprocess.run(cmd.split(" "))
+
+"""
 from subprocess import Popen
 from concurrent.futures import ThreadPoolExecutor
 
@@ -50,3 +55,4 @@ def run_command(command):
 with ThreadPoolExecutor(max_workers=max_processes) as executor:
     for command in commands:
         processes.append(executor.submit(run_command, command))
+"""

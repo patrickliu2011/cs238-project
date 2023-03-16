8d189eb41c180caa6aee237a8de860a453eb04a4
Kelechi Uhegbu <34874074+kelechiu10@users.noreply.github.com>
2023-03-16 02:46:20 -0700

    attempts at value iteration and policty iteration

---
+++ kc_valueiter.py
@@ -0,0 +1,124 @@
+import numpy as np
+import gymnasium as gym
+
+def get_size(shape):
+    size = 1
+    for i in shape:
+        size *= i
+    return size
+
+def run_episode(env, policy, gamma = 1.0, render = False):
+    """ Evaluates policy by using it to run an episode and finding its
+    total reward.
+    args:
+    env: gym environment.
+    policy: the policy to be used.
+    gamma: discount factor.
+    render: boolean to turn rendering on/off.
+    returns:
+    total reward: real value of the total reward recieved by agent under policy.
+    """
+    obs, info = env.reset()
+    total_reward = 0
+    step_idx = 0
+    while True:
+        if render:
+            env.render()
+        obs, reward, terminated, truncated , _ = env.step(int(policy[obs]))
+        total_reward += (gamma ** step_idx * reward)
+        step_idx += 1
+        done = terminated or truncated
+        if done:
+            break
+    return total_reward
+
+
+def evaluate_policy(env, policy, gamma = 1.0,  n = 100):
+    """ Evaluates a policy by running it n times.
+    returns:
+    average total reward
+    """
+    scores = [
+            run_episode(env, policy, gamma = gamma, render = False)
+            for _ in range(n)]
+    return np.mean(scores)
+
+def extract_policy(V, gamma = 1.0):
+    """ Extract the policy given a value-function """
+    sh = get_size(env.env.unwrapped.desc.astype(str).shape)
+    policy = np.zeros(sh)
+    for s in range(sh):
+        q_sa = np.zeros(env.action_space.n)
+        for a in range(env.action_space.n):
+            # for next_sr in env.P[s][a]:
+            #     # next_sr is a tuple of (probability, next state, reward, done)
+            #     p, s_, r, _ = next_sr
+            #     q_sa[a] += (p * (r + gamma * V[s_]))
+            q_sa[a] = sum([p * (r + gamma * V[s_]) for p, s_, r, _ in  env.P[s][a]])
+        policy[s] = np.argmax(q_sa)
+    return policy
+
+
+def value_iteration(env, gamma = 1.0):
+    """ Value-iteration algorithm """
+    sh = get_size(env.env.unwrapped.desc.astype(str).shape)
+    V = np.zeros(sh)  # initialize value-function
+    max_iterations = 100000
+    eps = 1e-20
+    for i in range(max_iterations):
+        prev_v = np.copy(V)
+        for s in range(sh):
+            q_sa = [sum([p*(r + prev_v[s_]) for p, s_, r, _ in env.P[s][a]]) for a in range(env.action_space.n)]
+            V[s] = max(q_sa)
+        if (np.sum(np.fabs(prev_v - V)) <= eps):
+            print ('Value-iteration converged at iteration# %d.' %(i+1))
+            break
+    return V
+
+def compute_policy_v(env, policy, gamma=1.0):
+    """ Iteratively evaluate the value-function under policy.
+    Alternatively, we could formulate a set of linear equations in iterms of v[s]
+    and solve them to find the value function.
+    """
+    sh = get_size(env.env.unwrapped.desc.astype(str).shape)
+    v = np.zeros(sh)
+    eps = 1e-10
+    i = 0
+    while True:
+        prev_v = np.copy(v)
+        for s in range(sh):
+            policy_a = policy[s]
+            v[s] = sum([p * (r + gamma * prev_v[s_]) for p, s_, r, _ in env.P[s][policy_a]])
+        if (np.sum((np.fabs(prev_v - v))) <= eps):
+            # value converged
+            break
+        i += 1
+    return v
+
+def policy_iteration(env, gamma = 1.0):
+    """ Policy-Iteration algorithm """
+    sh = get_size(env.env.unwrapped.desc.astype(str).shape)
+    policy = np.random.choice(env.action_space.n , size=(sh))  # initialize a random policy
+    max_iterations = 200000
+    gamma = 1.0
+    for i in range(max_iterations):
+        old_policy_v = compute_policy_v(env, policy, gamma)
+        new_policy = extract_policy(old_policy_v, gamma)
+        if (np.all(policy == new_policy)):
+            print ('Policy-Iteration converged at step %d.' %(i+1))
+            break
+        policy = new_policy
+    return policy
+
+if __name__ == '__main__':
+    env_name  = 'FrozenLake-v1'
+    gamma = 1
+    env = gym.make(env_name, is_slippery=True) #render_mode="human")
+    optimal_v = value_iteration(env, gamma)
+    policy = extract_policy(optimal_v, gamma)
+    policy_score = evaluate_policy(env, policy, gamma, n=1000)
+    print('Policy average score = ', policy_score)
+
+    optimal_policy = policy_iteration(env, gamma = 1.0)
+    scores = evaluate_policy(env, optimal_policy, gamma = 1.0)
+    print('Average scores = ', np.mean(scores))

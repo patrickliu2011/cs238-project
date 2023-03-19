9a359f2c5ff972bb1a5b3d6a3cc661a5fab044f5
Kelechi Uhegbu <34874074+kelechiu10@users.noreply.github.com>
2023-03-18 23:33:24 -0700

    KC has attempted to add value iteration as part of the suggestions. Apologies if it broke :D

--- frozenlake_env.py
+++ frozenlake_env.py
@@ -1,9 +1,10 @@
 import numpy as np
-import gym.spaces as spaces
-import gym
-import copy 
+import gymnasium.spaces as spaces
+import gymnasium as gym
+import copy
 
 import frozenlake_utils as fl
+import iter_utils
 
 class CustomFrozenLakeEnv(gym.Env):
     """Custom Environment that follows gym interface"""
@@ -11,7 +12,7 @@ class CustomFrozenLakeEnv(gym.Env):
     STATE_TYPES = ["embedded_map"]
     OBSCURE_TYPES = [None, "neighbor"]
 
-    def __init__(self, env_kwargs, env_data_kwargs, 
+    def __init__(self, env_kwargs, env_data_kwargs,
                  state_type="embedded_map", obscure_type=None,
                  guide_kwargs=None):
         super(CustomFrozenLakeEnv, self).__init__()
@@ -28,8 +29,8 @@ class CustomFrozenLakeEnv(gym.Env):
 
         self.action_space = spaces.discrete.Discrete(4)
         self._obs_map_shape = (
-            self._env_data["nrows"], 
-            self._env_data["ncols"], 
+            self._env_data["nrows"],
+            self._env_data["ncols"],
             len(self._env_data["tile_types"]),
         )
         self._suggestion_dim = 5
@@ -58,7 +59,7 @@ class CustomFrozenLakeEnv(gym.Env):
             x, y = fl.pos2coord(self._env.unwrapped.s, self._env_data)
             for i, j in np.ndindex((3, 3)):
                 coord = (int(x + i - 1), int(y + j - 1))
-                if not all(0 <= c < s for c, s in zip(coord, self._observed.shape)): 
+                if not all(0 <= c < s for c, s in zip(coord, self._observed.shape)):
                     continue
                 self._observed[coord] = True
         else:
@@ -71,16 +72,21 @@ class CustomFrozenLakeEnv(gym.Env):
             return observation_map[0] * self._observed[..., None]
         else:
             raise NotImplementedError("Invalid obscure type")
-        
+
     def _reset_suggestion(self):
         if self._guide_kwargs is None or self._guide_kwargs["type"] is None:
             pass
+        elif self._guide_kwargs["type"] == "vi": #value iteration
+            optimal_v = iter_utils.value_iteration(self._env, self._guide_kwargs.get("gamma", 0.9))
+            self._optimal_policy = iter_utils.extract_policy(self._env, optimal_v)
         else:
             raise NotImplementedError("Invalid guide type")
 
     def _update_suggestion(self, observation_map):
         if self._guide_kwargs is None or self._guide_kwargs["type"] is None:
             pass
+        elif self._guide_kwargs["type"] == "vi":
+            self._suggestion = self._optimal_policy[self._env.unwrapped.s]
         else:
             raise NotImplementedError("Invalid guide type")
 
@@ -89,6 +95,8 @@ class CustomFrozenLakeEnv(gym.Env):
             suggestion = np.zeros(self._suggestion_dim, dtype=np.uint8)
             suggestion[0] = 1
             return 0
+        elif self._guide_kwargs["type"] == "vi":
+            return self._suggestion + 1
         else:
             raise NotImplementedError("Invalid guide type")
 
@@ -96,7 +104,7 @@ class CustomFrozenLakeEnv(gym.Env):
         observation, reward, terminated, truncated, info = self._env.step(action)
         observation = fl.state_to_observation(observation, self._env_data, mode=self._state_type)
         obs_map = observation.astype(np.uint8).reshape(self._obs_map_shape)
-        
+
         self._update_obscuration(obs_map)
         obs_map = self._get_obscured_obs_map(obs_map)
 
@@ -106,15 +114,16 @@ class CustomFrozenLakeEnv(gym.Env):
         observation = {"map": obs_map, "suggestion": suggestion}
         done = terminated or truncated
         self.done = done
-        return observation, reward, done, info
-    
+        return observation, reward, terminated, truncated, info
+        # return observation, reward, done, info
+
     def reset(self):
         self._env = fl.get_env(**self._env_kwargs)
         self._env_data = fl.get_env_data(self._env, **self._env_data_kwargs)
         observation, info = self._env.reset()
         obs_map = fl.state_to_observation(observation, self._env_data, mode=self._state_type)
         obs_map = obs_map.astype(np.uint8).reshape(self._obs_map_shape)
-        
+
         self._reset_obscuration()
         self._update_obscuration(obs_map)
         obs_map = self._get_obscured_obs_map(obs_map)
@@ -125,8 +134,8 @@ class CustomFrozenLakeEnv(gym.Env):
 
         observation = {"map": obs_map, "suggestion": suggestion}
         self.done = False
-        return observation  # reward, done, info can't be included
-    
+        return observation, info  # reward, done, info can't be included
+
     def render(self, mode='human'):
         if not self.done:
             self._env.render()
